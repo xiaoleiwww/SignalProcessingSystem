84ea23e5ebc819f30b785c43657d85e378db43a7
xiaoleiwww <39595279+xiaoleiwww@users.noreply.github.com>
2024-03-22 15:14:53 +0800

    Update fft.py

--- generator/fft.py
+++ generator/fft.py
@@ -2,13 +2,13 @@
 """
 Created on Fri Mar 22 14:50:35 2024
 
-@author: Lenovo
+@author: xiaoleiwww
 """
 
 # import fourier_transform
 from sympy import fourier_transform, exp, sin
 from sympy.abc import x, k
- 
+
 # Using fourier_transform() method
 gfg = fourier_transform(exp(-x**2), x, k)
 gfg = fourier_transform(sin(x**2), x, k)
